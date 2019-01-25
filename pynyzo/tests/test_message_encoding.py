a74aa1fea93c28e28d36b72491a2ee7b69c1315a
EggPool <dev@eggpool.net>
2019-01-25 17:05:09 +0100

    first tests

---
+++ pynyzo/tests/test_message_encoding.py
@@ -0,0 +1,21 @@
+import pytest
+import sys
+
+sys.path.append('../pynyzo')
+from messages.statusresponse import StatusResponse
+
+
+def test_status_1(verbose=False):
+    strings = ['line1', 'line2']
+    message = StatusResponse(lines=strings)
+    if verbose:
+        print(message.get_bytes())
+    assert message.get_bytes() == b'\x02\x05\x00line1\x05\x00line2'
+    message2 = StatusResponse(buffer=message.get_bytes())
+    if verbose:
+        print(message2.get_lines())
+    assert str(message2.get_lines()) == "['line1', 'line2']"
+
+
+if __name__ == "__main__":
+    test_status_1(True)
