ec319f176b3135d44a0d8973b8ab93e154137b9b
EggPool <dev@eggpool.net>
2019-01-28 09:49:54 +0100

    bump version, history

--- pynyzo/setup.py
+++ pynyzo/setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/EggPool/pynyzo',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
